994b11bee40f15ebb6b4bf1ca6c4659d966b7f8a
zhongyu <931564975@qq.com>
2020-06-08 13:57:29 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: zhongyu
+ * @Date: 2020-06-08 12:07:20
+ * @LastEditors: zhongyu
+ * @LastEditTime: 2020-06-08 13:49:45
+--> 
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
